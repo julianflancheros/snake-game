d620555526b28c7e7f7783c59e5b22e7180c74df
julianflancheros <jflancherosm@unal.edu.co>
2021-10-25 18:03:26 -0500

    Creacion de archivos bascisos , html y js, utilizacion del j5.js

---
+++ principal.js
@@ -0,0 +1,16 @@
+// constantes de escenario del juego 
+const COLUMNS = 60;
+const ROWS = 30;
+const SIDE = 20;
+const WITDH_CANVAS = COLUMNS * SIDE;
+const HEIGHT_CANVAS = ROWS * SIDE;
+
+ // Funcion para dibujar el tamaño del canvas
+function setup() {
+    createCanvas(750, 500);
+}
+
+// Ciclo que se repite
+function draw() {
+    background('#979325');
+}
