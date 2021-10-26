f72cd0d014448dbb03c94439b28aa9c836e53997
julianflancheros <jflancherosm@unal.edu.co>
2021-10-26 01:09:44 -0500

    Realizacion primera parte del programa, movimiento automatico basado en una direccion, reconocimiento de las teclas, funcion de pausa basico

--- principal.js
+++ principal.js
@@ -5,12 +5,84 @@ const SIDE = 20;
 const WITDH_CANVAS = COLUMNS * SIDE;
 const HEIGHT_CANVAS = ROWS * SIDE;
 
- // Funcion para dibujar el tamaño del canvas
-function setup() {
-    createCanvas(750, 500);
+// variables de la serpiente
+xSerpiente = SIDE;
+ySerpiente = SIDE;
+let dir ={
+    x: 0,
+    y: 0
 }
 
-// Ciclo que se repite
+ // Funcion para dibujar el tamaño del canvas
+function setup() {
+    createCanvas(WITDH_CANVAS, HEIGHT_CANVAS)
+}
+
+// Ciclo que se repite 
 function draw() {
-    background('#979325');
+    background(170, 204, 102);
+    drawSnake();
+}
+
+function update(){  
+    xSerpiente += dir.x;
+    ySerpiente += dir.y;
+}
+
+
+function drawSnake(){
+    rect(xSerpiente,ySerpiente,SIDE, SIDE);
+    update();
+    sides();
+}
+
+function sides(){
+    // Entrada en los bordes en el eje x
+    if (xSerpiente >= WITDH_CANVAS){
+        xSerpiente = 0;
+    }
+    else if(xSerpiente < 0){
+        xSerpiente = WITDH_CANVAS;
+    }      
+    // Entrada en los bordes en el eje y
+    if (ySerpiente >= HEIGHT_CANVAS){
+        ySerpiente = 0;
+    }
+    else if(ySerpiente < 0){
+        ySerpiente = HEIGHT_CANVAS;
+    }
+}
+
+function keyPressed() {
+    if (keyCode == UP_ARROW){
+        ySerpiente -=10;
+        dir ={
+            x:0,
+            y:-1,
+        }
+    }
+    else if (keyCode == DOWN_ARROW){
+        ySerpiente +=10;
+        dir ={
+            x:0,
+            y:1,
+        }
+    }
+    else if (keyCode == LEFT_ARROW){
+        xSerpiente -=10;
+        dir ={
+            x:-1,
+            y:0,
+        }
+    }
+    else if (keyCode == RIGHT_ARROW){
+        xSerpiente +=10;
+        dir ={
+            x:1,
+            y:0,
+        }
+    }
+    if (keyCode == ESCAPE){
+        alert("PAUSE");
+    }
 }
