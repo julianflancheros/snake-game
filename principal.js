7e2544e3dd4af533ee3a5c0ccaa8b84691cdd714
julianflancheros <jflancherosm@unal.edu.co>
2021-11-13 23:57:24 -0500

    Simplificación del código, llamado nuevo al objeto snake y sus metodos, comentar y eliminar funciones o variables no necesarias

--- principal.js
+++ principal.js
@@ -3,14 +3,11 @@ const WITDH_CANVAS = 750;
 const HEIGHT_CANVAS = 500;
 
 // variables de la serpiente
-let food
+let food;
 let gap = 25;
-let xSerpiente = WITDH_CANVAS/2;
-let ySerpiente = HEIGHT_CANVAS/2;
-let dir ={
-    x: 0,
-    y: 0
-}
+// let xSerpiente = WITDH_CANVAS/2;
+// let ySerpiente = HEIGHT_CANVAS/2;
+
 let score = 0;
 let eat = new Audio('./assets/sounds/eat.mp3');
 let game_over = new Audio('./assets/sounds/game_over.mp3');
@@ -28,6 +25,7 @@ function setup() {
     textSize(25);
     textStyle(BOLD);
     food = new Food();
+    snake = new Snake();
     frameRate(10);
 }
 
@@ -52,19 +50,22 @@ function draw() {
             rect(j, i, gap, gap);
         }
     }
-    drawSnake();
-    line(0, 40, width, 40);
     noFill();
     // strokeWeight(4);
     stroke(43, 51, 25);
+    line(0, 40, width, 40);
     rect(0, 2*gap, width, height-gap);
     // noStroke();
-    if (food.x == xSerpiente && food.y == ySerpiente){
+    if (snake.collision(food)){
         eat.play();
         food.eat();
         score += 7;
     }
+
     food.show();
+    snake.show();
+    snake.move();
+    sides();
 
     // show score points in the display
     fill(43, 51, 25);
@@ -78,68 +79,38 @@ function draw() {
         text(int(score),  5, gap);
     }
 
-    
-}
-
-function update(){  
-    xSerpiente += dir.x;
-    ySerpiente += dir.y;
-}
-
-function drawSnake(){
-    fill("white");
-    rect(xSerpiente, ySerpiente, gap, gap);
-    update();
-    sides();
 }
 
 function sides(){
     // Entrada en los bordes en el eje x
-    if (xSerpiente >= WITDH_CANVAS){
-        xSerpiente = 0;
+    if (snake.x >= WITDH_CANVAS){
+        snake.x = 0;
     }
-    else if(xSerpiente < 0){
-        xSerpiente = WITDH_CANVAS;
+    else if(snake.x < 0){
+        snake.x = WITDH_CANVAS;
     }      
     // Entrada en los bordes en el eje y
-    if (ySerpiente >= HEIGHT_CANVAS){
-        ySerpiente = 50;
+    if (snake.y >= HEIGHT_CANVAS){
+        snake.y = 50;
     }
-    else if(ySerpiente < 50){
-        ySerpiente = HEIGHT_CANVAS;
+    else if(snake.y < 50){
+        snake.y = HEIGHT_CANVAS;
     }
 }
 
 function keyPressed() {
-    if (keyCode == UP_ARROW){
-        ySerpiente -=gap;
-        dir ={
-            x:0,
-            y:-gap,
-        }
+    if (keyCode == UP_ARROW && snake.dir != 'down'){
+        snake.dir = 'up';
     }
-    else if (keyCode == DOWN_ARROW){
-        ySerpiente +=gap;
-        dir ={
-            x:0,
-            y:gap,
-        }
+    else if (keyCode == DOWN_ARROW && snake.dir != 'up'){
+        snake.dir = 'down';
     }
-    else if (keyCode == LEFT_ARROW){
-        xSerpiente -=gap;
-        dir ={
-            x:-gap,
-            y:0,
-        }
+    else if (keyCode == LEFT_ARROW && snake.dir != 'right'){
+        snake.dir = 'left';
     }
-    else if (keyCode == RIGHT_ARROW){
-        xSerpiente +=gap;
-        dir ={
-            x:gap,
-            y:0,
-        }
-    }
-    if (keyCode == ESCAPE){
+    else if (keyCode == RIGHT_ARROW && snake.dir != 'left'){
+        snake.dir = 'right';
+    }else if (keyCode == ESCAPE){
         alert("PAUSE");
     }
 }
